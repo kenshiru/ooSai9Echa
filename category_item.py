8fc3068300c3a7277808ac1f4fa71e7946347f9d
Aleksandr Mochalov <kenshi2150@gmail.com>
2018-12-30 23:05:44 +0300

    Initial

---
+++ category_item.py
@@ -0,0 +1,18 @@
+import re
+
+
+class CategoryItem:
+    REPLACE_REGEX = re.compile('[^a-zA-Zа-яА-Я ]')
+
+    def __init__(self, name):
+        self.name = name
+        self.name_words = self.get_name_words(name)
+
+    @classmethod
+    def get_name_words(cls, name):
+        prepared_name = cls.REPLACE_REGEX.sub('', name)
+        return set(prepared_name.lower().split(' '))
+
+    def check(self, name):
+        checkable_name_words = self.get_name_words(name)
+        return len(self.name_words.difference(checkable_name_words)) == 0
---
+++ tests/test_category_item.py
@@ -0,0 +1,24 @@
+import unittest
+from category_item import CategoryItem
+
+
+class TestCategoryItem(unittest.TestCase):
+
+    def test_init(self):
+        self.assertEqual(
+            CategoryItem('Деревья на садовом кольце').name_words,
+            set(['деревья', 'на', 'садовом', 'кольце'])
+        )
+
+        self.assertEqual(
+            CategoryItem('Деревья на ,садовом кольце').name_words,
+            set(['деревья', 'на', 'садовом', 'кольце'])
+        )
+        self.assertEqual(
+            CategoryItem('ДеРеВьЯ на ,саДоВОМ кольце').name_words,
+            set(['деревья', 'на', 'садовом', 'кольце'])
+        )
+
+    def test_check(self):
+        self.assertTrue(CategoryItem('рецепт борща').check('Борща любимого рецепт'))
+        self.assertFalse(CategoryItem('рецепт борща корейского').check('Борща любимого рецепт'))
